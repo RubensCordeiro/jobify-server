6119b076c779882964406a456c8bdcf64b8d3f48
Rubens Cordeiro <rubensccordeiro@outlook.com>
2023-02-09 17:21:07 -0300

    Jobs controller initial structure

---
+++ app/controllers/api/v1/jobs_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class JobsController < ApplicationController
+      def index; end
+
+      def create; end
+
+      def show; end
+
+      def update; end
+
+      def destroy; end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :jobs
+
       scope :auth do
-        post '/register', to: "authentication#register"
-        post '/login', to: "authentication#login"
-        patch '/update_user', to: "authentication#update_user"
+        post '/register', to: 'authentication#register'
+        post '/login', to: 'authentication#login'
+        patch '/update_user', to: 'authentication#update_user'
       end
     end
   end
