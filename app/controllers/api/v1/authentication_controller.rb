802e08c6439fba16ee806c8b72b92bfc44f4ef37
Rubens Cordeiro <rubensccordeiro@outlook.com>
2023-02-09 17:14:14 -0300

    Initial structure for auth controller

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module V1
+    class AuthenticationController < ApplicationController
+      def register
+      end
+      
+      def login
+      end
+
+      def update_user
+      end
+    end
+  end
+end    
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      scope :auth do
+        post '/register', to: "authentication#register"
+        post '/login', to: "authentication#login"
+        patch '/update_user', to: "authentication#update_user"
+      end
+    end
+  end
 end
